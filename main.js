37c55da06fc287339441e4afe6b14f7d1e020faf
oodysseus <oodysseus@163.com>
2019-05-07 22:32:09 +0800

    第二个版本

--- main.js
+++ main.js
@@ -1,17 +1,61 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-
-let n = 1
+let n
+init()
 
 setInterval(()=>{
-  $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+  makeLeave(getImage(n))
     .one('transitionend', (e)=>{
-      $(e.currentTarget).removeClass('leave').addClass('enter')
+      makeEnter($(e.currentTarget))
     })
-  $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+    makeCurrent(getImage(n+1))
   n++
+  n=x(n)
+  console.log(n)
 },3000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function init(){
+  n = 1
+  $('.images > img:nth-child(1)').addClass('current')
+  $('.images > img:nth-child(2)').addClass('enter')
+  $('.images > img:nth-child(3)').addClass('enter')
+}
+
+function getImage(n){
+  return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function makeCurrent($node){
+  return $node.removeClass('enter').addClass('current')
+}
+
+function makeLeave($node){
+  return $node.removeClass('current').addClass('leave')
+}
+
+function makeEnter($node){
+  return $node.removeClass('leave').addClass('enter')
+}
 
 function x(n){
   n=n%3
